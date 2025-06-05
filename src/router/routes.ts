a1fb6843b68f05682f91a922969bcfea6e6b4afa
Romário Lima <devromariolima@gmail.com>
2025-06-05 13:54:29 -0400

    Adiciona arquivo de rotas para gerenciamento de criptomoedas

---
+++ src/router/routes.ts
@@ -0,0 +1,43 @@
+// import { RouteRecordRaw } from 'vue-router'
+// import { useCryptocurrenciesStore } from '..cryptocurrencies-Store/store/'
+// import { cryptocurrencies } from 'src/types/cryptocurrencies'
+
+// const routes: RouteRecordRaw[] = [
+//   {
+//     path: '/',
+//     component: () => import('layouts/MainLayout.vue'),
+//     meta: {
+//       requireAuth: true
+//     },
+//     children: [
+//       {
+//         path: '',
+//         component: () => import('pages/IndexPage.vue'),
+//         name: 'home'
+//       },
+
+//     ]
+//   },
+//   {
+//     path: '/produtos',
+//     component: () => import('layouts/MainLayout.vue'),
+//     children: [
+//       {
+//         path: '',
+//         component: () => import(''),
+//         name: 'cryptocurrencies.listar'
+//       },
+//       {
+//         path: '/cryptocurrencies/visualizar/:id',
+//         component: () => import(''),
+//         name: 'cryptocurrencies.editar'
+//       }
+//     ]
+//   },
+//   {
+//     path: '/:catchAll(.*)*',
+//     component: () => import('pages/ErrorNotFound.vue')
+//   }
+// ]
+
+// export default routes
