24baf4bb1b9115bfdd95bd986cbbba12d3d65fcb
Romário Lima <devromariolima@gmail.com>
2025-06-05 13:45:35 -0400

    Adiciona estrutura inicial da store de criptomoedas com Pinia

---
+++ src/store/cryptocurrencies-Store.ts
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+// import {cryptocurrenciesService,} from 'src/types/cryptocurrenciesService'
+// import cryptocurrenciesService, { cryptocurrenciesService } from '../service/cryptocurrenciesService'
+// import { ref } from 'vue'
+
+export const usePermissoesStore = defineStore('Cryptocurrencies-store', () => {
+//   const cryptocurrenciesService = ref<cryptocurrenciesService[]>([])
+
+
+
+
+//   async function Cryptocurrencies (): Promise<void> {
+//     const response = await Service.Cryptocurrencies()
+//     if (response.data?.sistemas) {
+//       teste.value = response.data?.teste[0].teste
+//     }
+//     return response
+//   }
+
+  return {
+    // recursos,
+    // Cryptocurrencies,
+  }
+})
