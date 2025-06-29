db2d41fa0534900ea20bc3fc92328b835e62201e
Romário Lima <devromariolima@gmail.com>
2025-06-29 19:02:04 -0400

    chore: update dependencies and add support for multiple platforms in yarn.lock
    - Added new platform-specific packages for esbuild and rollup, including support for various architectures (e.g., android-arm64, darwin-arm64, linux-arm64, etc.).
    - Updated existing packages to their latest versions, including @vue/devtools-api and sass-embedded.
    - Removed duplicate version specifications for some packages to streamline the lock file.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,43 +1,26 @@
-// import { RouteRecordRaw } from 'vue-router'
-// import { useCryptocurrenciesStore } from '..cryptocurrencies-Store/store/'
-// import { cryptocurrencies } from 'src/types/cryptocurrencies'
+import { RouteRecordRaw } from 'vue-router'
 
-// const routes: RouteRecordRaw[] = [
-//   {
-//     path: '/',
-//     component: () => import('layouts/MainLayout.vue'),
-//     meta: {
-//       requireAuth: true
-//     },
-//     children: [
-//       {
-//         path: '',
-//         component: () => import('pages/IndexPage.vue'),
-//         name: 'home'
-//       },
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/cryptocurrencies',
+    component: () => import('../App.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('../App.vue'),
+        name: 'cryptocurrencies.listar'
+      },
+    //   {
+    //     path: '/cryptocurrencies/visualizar/:id',
+    //     component: () => import(''),
+    //     name: 'cryptocurrencies.editar'
+    //   }
+    ]
+  },
+  {
+    path: '/:catchAll(.*)*',
+    // component: () => import('pages/ErrorNotFound.vue')
+  }
+]
 
-//     ]
-//   },
-//   {
-//     path: '/cryptocurrencies',
-//     component: () => import('layouts/MainLayout.vue'),
-//     children: [
-//       {
-//         path: '',
-//         component: () => import(''),
-//         name: 'cryptocurrencies.listar'
-//       },
-//       {
-//         path: '/cryptocurrencies/visualizar/:id',
-//         component: () => import(''),
-//         name: 'cryptocurrencies.editar'
-//       }
-//     ]
-//   },
-//   {
-//     path: '/:catchAll(.*)*',
-//     component: () => import('pages/ErrorNotFound.vue')
-//   }
-// ]
-
-// export default routes
+export default routes
