9424def466fb47e7a9d9e7ee81266e7ebe9a08a5
Romário Lima <devromariolima@gmail.com>
2025-06-29 19:14:36 -0400

    feat: adiciona função GetCryptocurrenciesByid e refatora o estado para incluir CoinbaseId

--- src/store/cryptocurrencies-Store.ts
+++ src/store/cryptocurrencies-Store.ts
@@ -2,9 +2,11 @@ import { defineStore } from 'pinia'
 import type BitcoinData from '../types/Coinbase'
 import cryptocurrenciesService from '../service/cryptocurrenciesService'
 import { ref } from 'vue'
+import type { CryptoData } from '../types/CoinBaseId'
 
 export const useCryptocurrenciesStore = defineStore('Cryptocurrencies-store', () => {
   const Coinbase = ref<BitcoinData[]>([])
+  const CoinbaseId = ref<CryptoData[]>([])
 
 
 
@@ -16,8 +18,16 @@ async function GetCryptocurrencies(): Promise<BitcoinData[]> {
   return data
 }
 
+async function GetCryptocurrenciesByid(nome: string): Promise<CryptoData[]> {
+  const data = await cryptocurrenciesService.GetCryptocurrenciesByid(nome)
+  CoinbaseId.value = data
+  console.log('Dados da moedas', Coinbase.value)
+  return data
+}
+
   return {
     Coinbase,
     GetCryptocurrencies,
+    GetCryptocurrenciesByid
   }
 })
