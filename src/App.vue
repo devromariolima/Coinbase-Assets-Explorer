65b326d6d77a5ec81d3e088c60ab0fcde8e95909
Romário Lima <devromariolima@gmail.com>
2025-06-29 18:49:25 -0400

    Refatora o componente App.vue para utilizar o novo componente CriptoList e remove o componente HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,13 @@
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
-import { useCryptocurrenciesStore } from './store/cryptocurrencies-Store'
-
-const loading = ref<boolean>(true)
-const useStore = useCryptocurrenciesStore()
-const coinbaseList = computed(() => useStore.Coinbase)
-
-async function getData () {
-  try {
-    loading.value = true 
-    await useStore.GetCryptocurrencies()
-  } catch (error) {
-    if (error instanceof Error) {
-      // notify.error(error.message ?? 'Erro ao buscar produtos')
-    }
-  } finally {
-    loading.value = false
-  }
-}
-
-onMounted(() => getData())
+import CryptoList from '../src/components/CriptoList.vue'
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
     <q-page-container>
       <q-page class="bg-grey-3">
-        <div class="q-pa-md">
-          <div class="text-h5 text-weight-bold text-dark q-mb-md">
-            GERENCIAR CRIPTOMOEDAS
-            <q-btn
-              class="float-right"
-              icon="refresh"
-              label="Atualizar"
-              color="primary"
-              @click="getData()"
-            />
-          </div>
-
-          <q-separator class="q-my-md" />
-
-          <q-card flat class="bg-white">
-            <q-list
-              v-if="!loading && coinbaseList.length > 0"
-              bordered
-              separator
-            >
-              <q-item
-                v-for="coin in coinbaseList"
-                :key="coin.id"
-                class="q-py-sm"
-              >
-                <q-item-section avatar>
-                  <q-avatar size="md" color="grey-4">
-                    <img 
-                      :src="coin.image_url" 
-                      v-if="coin.image_url" 
-                      style="width: 70px; height: 70px; object-fit: contain"
-                    >
-                    <q-icon v-else name="payments" color="grey-7" />
-                  </q-avatar>
-                </q-item-section>
-                
-                <q-item-section>
-                  <q-item-label class="text-weight-medium text-dark">{{ coin.name }}</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-
-            <div
-              v-if="loading"
-              class="q-gutter-y-md q-mt-md"
-            >
-              <q-skeleton height="50px" />
-              <q-skeleton height="50px" />
-              <q-skeleton height="50px" />
-            </div>
-
-            <q-banner
-              v-if="!loading && coinbaseList.length === 0"
-              class="bg-yellow-2 text-dark"
-            >
-              Não foram encontradas criptomoedas.
-            </q-banner>
-          </q-card>
-        </div>
+        <CryptoList />
       </q-page>
     </q-page-container>
   </q-layout>
 </template>
-
-<style scoped>
-.q-item {
-  min-height: 100px;
-}
-
-.q-item:hover {
-  background-color: #f8f9fa;
-}
-</style>
