b6df16cef3d3df0ce5874faf36727f799caba633
Romário Lima <devromariolima@gmail.com>
2025-06-30 18:51:11 -0400

    Update CryptoList.vue

--- src/components/CryptoList.vue
+++ src/components/CryptoList.vue
@@ -38,7 +38,7 @@ onMounted(() => getData())
   <q-page padding class="bg-grey-2 justify-center">
     <div class="col-12 col-md-8 col-lg-6">
       <div class="text-h5 text-weight-bold text-black q-mb-md text-center">
-        informe a criptomoedas desejada
+        Coinbase Assets Explorer
       </div>
 
       <q-input
