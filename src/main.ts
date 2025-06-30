70db25655ee17709df1496131da98c1d2174ff09
Romário Lima <devromariolima@gmail.com>
2025-06-29 20:00:21 -0400

    feat: adiciona configuração de roteamento com Vue Router no aplicativo

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 import './style.css'
 import App from './App.vue'
+import routes from '../src/router/routes'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 const app = createApp(App)
+app.use(router)
 
 app.use(Quasar, {
   plugins: {},
